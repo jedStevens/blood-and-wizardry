d428ee554930f33c58ad1c59c5d7e8e440ba6b62
Jed Stevens <jed.w.stevens@gmail.com>
2016-04-26 08:12:31 -0400

    Added assets

---
+++ ip-global.gd
@@ -0,0 +1,18 @@
+
+extends Label
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	set_fixed_process(true)
+
+func _fixed_process(delta):
+	var ip = Globals.get("ip")
+	var port =  Globals.get("port")
+	var is_host = Globals.get("host")
+	var client_count = get_node("..").clients.size()
+	set_text("IP: " + str(ip) + " : " + str(port) + "\nHost: "+ str(is_host) + "\nClient Count:" + str(client_count))
