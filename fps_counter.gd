1c7bdc9961005364dce79f4af30446c4f6b099bd
Jed Stevens <jed.w.stevens@gmail.com>
2016-05-04 12:17:20 -0400

    Camera Logic and basic Tilemap

--- fps_counter.gd
+++ fps_counter.gd
@@ -4,4 +4,4 @@ func _ready():
     set_process(true)
 
 func _process(d):
-    set_text(str(OS.get_frames_per_second()))
+    set_text("FPS:  "+str(OS.get_frames_per_second()))
---
+++ game_content/game/camera.gd
@@ -0,0 +1,19 @@
+
+extends Camera2D
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+
+func _ready():
+	set_fixed_process(true)
+
+func _fixed_process(delta):
+	var avg = Vector2(0,0)
+	var i = 0
+	for player in get_tree().get_nodes_in_group("players"):
+		avg += player.get_pos()
+		i += 1
+	avg = avg / i
+	
+	set_pos(avg)
